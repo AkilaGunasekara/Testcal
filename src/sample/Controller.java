b1cec772be3760d905558aa362bc0f36f9b8bf47
Dileesha Weliwaththa <64683688+dileeshaweliwaththa@users.noreply.github.com>
2021-06-09 10:28:28 +0530

    Did some Changes to make it Perfect Check it out.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -10,7 +10,7 @@ import javafx.scene.control.TextField;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class Controller implements Initializable {
+public class Controller {
     @FXML
     private TextField n1;
 
@@ -18,22 +18,13 @@ public class Controller implements Initializable {
     private TextField n2;
 
     @FXML
-    private Button addition;
-
-    @FXML
     private Label display;
 
-    @FXML
-    void caladdition(ActionEvent event){
-        int number1= Integer.parseInt(n1.getText());
-        int number2= Integer.parseInt(n2.getText());
+
+    public void caladdition(ActionEvent event){
+        float number1= Float.parseFloat(n1.getText());
+        float number2= Float.parseFloat((n2.getText()));
 
         display.setText(String.valueOf(number1+number2));
-    }
-
-
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-
     }
 }
